f2c11266e3bce643c24b29de722af47e0f94ec77
Cabrero Gutiérrez <rocz@gft.com>
2021-02-26 11:22:30 +0100

    added Lazyload

--- src/components/heroes/HeroCard.jsx
+++ src/components/heroes/HeroCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LazyLoad from 'react-lazyload';
 import { Link } from 'react-router-dom';
 import styles from '../../styles/card.module.css'
 
@@ -9,9 +10,11 @@ export const HeroCard = ({hero}) => {
     return(
         <div className={styles.card}>
             <div className={styles.image}>
+            <LazyLoad height={200}>
                 <img 
                 src={`./assets/heroes/${id}.jpg`}
                 alt={superhero}/>
+            </LazyLoad>
             </div>
             <div className={styles.body}>
                 <h5 className={styles.title}>{superhero}</h5>
